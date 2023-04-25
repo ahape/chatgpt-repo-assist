4559dd4a0f505de2a49c869c76358e744501a457
Alan Hape <ahape@brightmetrics.com>
2023-04-24 22:30:18 -0700

    progress but still don't werk

--- index.py
+++
@@ -1,7 +0,0 @@
-import deeplake
-import pandas as pd
-
-df = pd.read_csv("file.csv")
-deeplake_path = "hub://ahape/example"
-
-ds = deeplake.ingest_dataframe(df, deeplake_path, token = "eyJhbGciOiJIUzUxMiIsImlhdCI6MTY4MjM5Njg2MCwiZXhwIjoxNjg4MTg1NjE5fQ.eyJpZCI6ImFoYXBlIn0.2jk8Y6e8GqTQ2wbJ0H6--JIuQQfxmLX_E9Iagj3ik7lzeDwNSP_QuOgLWN161ZH-dau_t4ufukDRDwRVFVAHAA")
---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/local/bin/python3.11
+import os, sys, configparser
+
+config = configparser.ConfigParser()
+config.read("secrets.config")
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+arg = sys.argv[-1]
+
+def init():
+  os.environ["OPENAI_API_KEY"] = config["openai"]["api_key"]
+  os.environ["ACTIVELOOP_TOKEN"] = config["activeloop"]["token"]
+
+  embeddings = OpenAIEmbeddings()
+
+  root_dir = os.environ.get("ROOT", "/Users/alanhape/Downloads/roslyn-tools")
+  docs = []
+
+  for dirpath, dirnames, filenames in os.walk(root_dir):
+    for file in filenames:
+      try: 
+        loader = TextLoader(os.path.join(dirpath, file), encoding="utf-8")
+        docs.extend(loader.load_and_split())
+      except Exception as e:
+        print(e, file)
+
+  print(f"{len(docs)} docs collected")
+
+  text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+  texts = text_splitter.split_documents(docs)
+
+  username = config["activeloop"]["user"]
+  dataset = config["activeloop"]["dataset"]
+  dataset_path = f"hub://{username}/{dataset}"
+
+  print("Next: Performing the indexing process")
+
+  db = DeepLake(
+      dataset_path=dataset_path,
+      embedding_function=embeddings,
+      public=True)
+  db.add_documents(texts)
+
+def main():
+  db = Dataset(path=dataset_path, read_only=True, tensors=["embedding", "ids", "metadata", "text"])
+  print(db)
+
+if __name__ == "__main__":
+  if arg == "init":
+    init()
+  else:
+    main()
